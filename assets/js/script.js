dac02aa065dc3a102e3e606e760ccaa64d22adb6
alessandrodamato <alessandro042004@gmail.com>
2024-02-22 15:47:58 +0100

    [add] fizzbuzz condition in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 const limit = 100;
-let fizz = 'fizz'
+let fizz = 'Fizz'
+let buzz = 'Buzz'
 
 // ciclo da 1 a 100
 for (let i = 1; i <= limit; i++){
-  if (i % 3 === 0){
+  if ((!(i % 3)) && (!(i % 5))) {
+    console.log(fizz, buzz);
+  } else if (!(i % 5)) {
+    console.log(buzz);
+  } else if (!(i % 3)) {
     console.log(fizz);
+  } else {
+    console.log(i);
   }
-  console.log(i);
 }
